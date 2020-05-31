9af478daedce1b3fde73eb63c1510aed0f99d2d8
avinash12345786 <avikiitian7@gmail.com>
2020-05-31 12:55:53 +0530

    updating user profile and improving scss of home page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -18,6 +18,17 @@ User.findById(req.params.id,function(err,user){
 
 
 }
+// for updates
+module.exports.update=function(req,res){
+if(req.user.id==req.params.id){
+    User.findByIdAndUpdate(req.params.id,req.body,function(err,user){
+  return res.redirect('back');
+});
+
+}else{
+  return res.status(401).send('Unauthorized')
+}
+} 
 
 //render the signUp page
 module.exports.signUp=function(req,res){
