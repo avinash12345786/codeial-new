685867bc18e9daa5d466fe5e54b726ce9ce96957
avinash12345786 <avikiitian7@gmail.com>
2020-05-31 19:10:39 +0530

    adding more flash messages

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -6,23 +6,24 @@ const User=require('../models/user');
 //authentication using passport
 
 passport.use(new LocalStrategy({
-usernameField:'email'
+usernameField:'email',
+passReqToCallback:true
 
     },
 
-    function(email,password,done){
-        console.log(email,password);
+    function(req,email,password,done){
+       
 //find user & stablish the identity
 User.findOne({email:email},function(err,user){
 
 
     if(err){
-        console.log('error in finding user---->passport');
+        req.flash('error',err);
         return done(err);
     }
     if(!user || user.password !=password){
 
-        console.log('invalid username/passwrod');
+        req.flash('error','invalid username/passwrod');
         return done(null,false);
     }
     return done(null,user);
