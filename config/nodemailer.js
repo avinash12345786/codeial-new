7d02d2d83bed83de0de2fcaf9ceefa8fd439b7c9
avnash12345786 <avikiitian7@gmail.com>
2020-06-11 17:22:41 +0530

    setting up node mailer

---
+++ config/nodemailer.js
@@ -0,0 +1,33 @@
+const nodemailer=require('nodemailer');
+const ejs=require('ejs');
+const path=require('path');
+
+let transporter=nodemailer.createTransport({
+ service:'gmail',
+ post:'smtp.gmail.com',
+ port:'587',
+ auth:{
+     user:'avikiitian7',
+     pass:'BirlaKpit@123'
+ }
+
+
+});
+
+let renderTemplate=(data,relativePath)=>{
+    let mailHTML;
+ejs.renderFile(
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        function(err,template){
+            if(err){console.log('error in rendering  template');return}
+            mailHTML=template
+        }
+
+)
+    return mailHTML
+}
+module.exports={
+    transporter:transporter,
+    renderTemplate:renderTemplate
+}
